144fb3671316e9c02a1f481086a64fbc93f8dcbd
Nemechix <nemesi.jobs@gmail.com>
2023-02-01 17:07:25 +0000

    movimiento enemigo next

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ let torreta1 = document.getElementById("torreta1")
 let cells = document.querySelectorAll("#board td")
 let chosedTurret = ""
 let torreUP = "no"
-let money2 = document.getElementById("uiMoney")
-let money = parseInt(money2)
+let money = document.getElementById("uiMoney")
+let money2 = parseInt(money.innerHTML)
+console.log(typeof(money2))
 
 
 torreta1.onclick = function(){
@@ -20,11 +21,14 @@ cells.forEach((element) => {
 
     element.onclick = function(){
         
-    if(money >= 10)
+        if (money2 >= 10 && chosedTurret !== ""){
 
     console.log("work")
     element.setAttribute("id", chosedTurret)
-    money -= 10 
+    money2 = money2 - 10
+    money.innerHTML = money2
+
+    }
     }
 });
 
