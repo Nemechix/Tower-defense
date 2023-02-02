1ceb5038399b3d2234672c11fd887938fbf28c9e
Nemechix <nemesi.jobs@gmail.com>
2023-02-02 17:09:17 +0000

    movimientoterminar

--- enemy.js
+++ enemy.js
@@ -1,33 +1,143 @@
-var x = document.querySelector(".enemy1")
-var y = document.querySelector(".enemy1")
-var xT = document.querySelector(".row8 .col11")
-var yT = document.querySelector(".row8 .col11")
-
-function Enemy(hp, Down, Right, Left, Up, ) {
-    this.pos = [{ x: 10, y: 1 }]
-    this.hp = hp
-    this.sprite = document.querySelectorAll(".enemy1")
-
-    this.directionRight = Right
-    this.directionLeft = Left
-    this.directionUp = Up
-    this.directionDown = Down
-
-    Enemy.prototype.movePath = function(){
-
+function Enemy(hp, x, y, direction) {
+    this.pos = {
+        x: x,
+        y: y
     }
+    this.hp = hp
+    this.direction = 'right'
 
-    Enemy.prototype.receiveDamage = function (damage) {
-        this.hp -= damage
-        if (this.hp > 0) {
-            //console.log("stillAlive")
-        }
+    Enemy.prototype.movePath = function () {
+
+
+
+        if (direction === "right") {
+            let nextCell = document.querySelector(`.row${this.pos.x} .col${this.pos.y + 1}`);
+            let checkPath = nextCell.classList.contains("path")
+            if (checkPath == true) {
+                let enemy1 = document.querySelector(`.row${this.pos.x} .col${this.pos.y}`);
+                enemy1.classList.remove("enemy1")
+                nextCell.classList.add("enemy1")
+                this.pos.y++
+                console.log(enemy1)
+            } else if(checkeamos la celda a mano derecha) {
+                direction = 'down'
+            } else {
+                direction = 'up'
+            }
+
+        if (direction === "down") {
+            let nextCell = document.querySelector(`.row${this.pos.x + 1} .col${this.pos.y}`);
+            let checkPath = nextCell.classList.contains("path")
+            if (checkPath == true) {
+                let enemy1 = document.querySelector(`.row${this.pos.x} .col${this.pos.y}`);
+                enemy1.classList.remove("enemy1")
+                nextCell.classList.add("enemy1")
+                this.pos.y++
+                console.log(enemy1)
+            } else if (checkeamos la celda a mano derecha) {
+                direction = 'down'
+            } else {
+                direction = 'up'
+            }
+
+        if (direction === "right") {
+            let nextCell = document.querySelector(`.row${this.pos.x + 1} .col${this.pos.y}`);
+            let checkPath = nextCell.classList.contains("path")
+            if (checkPath == true) {
+                let enemy1 = document.querySelector(`.row${this.pos.x} .col${this.pos.y}`);
+                enemy1.classList.remove("enemy1")
+                nextCell.classList.add("enemy1")
+                this.pos.y++
+                console.log(enemy1)
+            } else if (checkeamos la celda a mano derecha) {
+                direction = 'down'
+            } else {
+                direction = 'up'
+            }
+
+            if (direction === "left") {
+                let nextCell = document.querySelector(`.row${this.pos.x + 1} .col${this.pos.y}`);
+                let checkPath = nextCell.classList.contains("path")
+                if (checkPath == true) {
+                    let enemy1 = document.querySelector(`.row${this.pos.x} .col${this.pos.y}`);
+                    enemy1.classList.remove("enemy1")
+                    nextCell.classList.add("enemy1")
+                    this.pos.y++
+                    console.log(enemy1)
+                } else if (checkeamos la celda a mano derecha) {
+                    direction = 'down'
+                } else {
+                    direction = 'up'
+                }
+
+
+
+
+
+
+
+}/*
+            else if (up === "up") {
+                let nextCellUp = document.querySelector(`.row${this.pos.x - 1} .col${this.pos.y}`);
+                var checkPathUp = nextCellUp.classList.contains("path")
+
+
+                if (checkPathUp == true) {
+                    let enemy1 = document.querySelector(`.row${this.pos.x} .col${this.pos.y}`);
+                    enemy1.classList.remove("enemy1")
+                    nextCellUp.classList.add("enemy1")
+                    this.pos.x--
+                    console.log(enemy1)
+
+                }
+
+                else if (down === "down") {
+                    let nextCelldown = document.querySelector(`.row${this.pos.x + 1} .col${this.pos.y}`);
+                    let checkPathdown = nextCelldown.classList.contains("path")
+
+                    if (checkPathdown == true) {
+                        let enemy1 = document.querySelector(`.row${this.pos.x} .col${this.pos.y}`);
+                        enemy1.classList.remove("enemy1")
+                        nextCelldown.classList.add("enemy1")
+                        this.pos.x++
+                        console.log(enemy1)
+                    }
+
+                }
+            }
+
+        
+         if (down === "down") {
+            let nextCelldown = document.querySelector(`.row${this.pos.x + 1} .col${this.pos.y}`);
+            let checkPathdown = nextCelldown.classList.contains("path")
+
+            if (checkPathdown == true && checkPathUp === false) {
+                let enemy1 = document.querySelector(`.row${this.pos.x} .col${this.pos.y}`);
+                enemy1.classList.remove("enemy1")
+                nextCelldown.classList.add("enemy1")
+                this.pos.x++
+                console.log(enemy1)
+            }
+
+
+
+
+            Enemy.prototype.receiveDamage = function (damage) {
+                this.hp -= damage
+                if (this.hp > 0) {
+                    //console.log("stillAlive")
+                }
+            }
+        } */
     }
 }
-var enemy1 = new Enemy(10, x, y)
 
+var enemy1 = new Enemy(10, 9, 0, "right")
+let anim = setInterval(() => {
+    enemy1.movePath()
+}, 200);
 
-console.log(enemy1.pos)
+//console.log(enemy1.pos)
 
 
 
@@ -44,7 +154,8 @@ Turret1.prototype.attack = function () {
 }
 
 
-torreta1 = new Turret1(5, xT, yT)
+
+//torreta1 = new Turret1(5, xT, yT)
 
 if (enemy1.posX === enemy1.posY) {
     enemy1.receiveDamage(torreta1.attack())
