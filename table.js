fbe13cdb3c898fea6c653a38aa0ab056a6a20404
Almacrh <alma_cruz_hernandez@outlook.com>
2023-02-04 17:33:30 +0100

    Remakeando

---
+++ table.js
@@ -0,0 +1,30 @@
+function table (){
+  const table = document.createElement("table")
+  const tbody = document.createElement("tbody")
+
+  for(let xCells = 0; xCells < 20; xCells++){
+    const row = document.createElement("tr")
+    
+    row.classList.add(`row${xCells}`)
+
+      for(let yCells = 0; yCells < 20; yCells++ ){
+        const cell = document.createElement("td")
+        const cellText = document.createTextNode(``)
+
+        cell.appendChild(cellText)
+        row.appendChild(cell)
+        cell.classList.add(`col${yCells}`)
+      }
+      tbody.appendChild(row)
+
+      
+  }
+  table.appendChild(tbody)
+  document.body.appendChild(table)
+  table.setAttribute("border", "1")
+
+
+  
+}
+
+table()
--- turrets.js
+++ turrets.js
@@ -1,54 +1,54 @@
-let torreta1 = document.getElementById("torreta1")
-let cells = document.querySelectorAll("#board td")
-let cellsEnemy = document.querySelectorAll("#board td")
-let path = document.querySelectorAll(".path")
+// let torreta1 = document.getElementById("torreta1")
+// let cells = document.querySelectorAll("#board td")
+// let cellsEnemy = document.querySelectorAll("#board td")
+// let path = document.querySelectorAll(".path")
 
-let chosedTurret = ""
-let torreUP = "no"
-let money = document.getElementById("uiMoney")
-let money2 = parseInt(money.innerHTML)
+// let chosedTurret = ""
+// let torreUP = "no"
+// let money = document.getElementById("uiMoney")
+// let money2 = parseInt(money.innerHTML)
 
-function Turret(dmg, x, y) {
-    this.dmg = dmg
-    this.sprite = document.getElementById("torreta1")
-    this.pos = {
-        x: x,
-        y: y
-    }
-    Turret.prototype.range = function () {
+// function Turret(dmg, x, y) {
+//     this.dmg = dmg
+//     this.sprite = document.getElementById("torreta1")
+//     this.pos = {
+//         x: x,
+//         y: y
+//     }
+//     Turret.prototype.range = function () {
         
-         let rangeRight = document.querySelector(`.row${this.pos.x} .col${this.pos.y + 1}`);
-        let rangeLeft = document.querySelector(`.row${this.pos.x} .col${this.pos.y - 1}`);
-        let rangeUp = document.querySelector(`.row${this.pos.x - 1} .col${this.pos.y}`);
-        let rangeDown = document.querySelector(`.row${this.pos.x + 1} .col${this.pos.y}`);
+//          let rangeRight = document.querySelector(`.row${this.pos.x} .col${this.pos.y + 1}`);
+//         let rangeLeft = document.querySelector(`.row${this.pos.x} .col${this.pos.y - 1}`);
+//         let rangeUp = document.querySelector(`.row${this.pos.x - 1} .col${this.pos.y}`);
+//         let rangeDown = document.querySelector(`.row${this.pos.x + 1} .col${this.pos.y}`);
 
-        if (rangeRight.classList.contains("enemy1")) {
-            console.log("hit") 
-        }
-    }
-}
+//         if (rangeRight.classList.contains("enemy1")) {
+//             console.log("hit") 
+//         }
+//     }
+// }
 
-let turret1 = new Turret(5)
+// let turret1 = new Turret(5)
 
 
-torreta1.onclick = function () {
-    chosedTurret = "torreta1"
-    console.log("work")  
-}
+// torreta1.onclick = function () {
+//     chosedTurret = "torreta1"
+//     console.log("work")  
+// }
 
-//Sistema de colocar torretas
-cells.forEach((element) => {
+// //Sistema de colocar torretas
+// cells.forEach((element) => {
 
-    element.onclick = function () {
-        if (!element.classList.contains("path")) {
-            if (money2 >= 10 && chosedTurret !== 1) {
+//     element.onclick = function () {
+//         if (!element.classList.contains("path")) {
+//             if (money2 >= 10 && chosedTurret !== 1) {
 
-                element.setAttribute("id", chosedTurret)
-                money2 = money2 - 10
-                money.innerHTML = money2
-                game.torreta.push(element)
+//                 element.setAttribute("id", chosedTurret)
+//                 money2 = money2 - 10
+//                 money.innerHTML = money2
+//                 game.torreta.push(element)
 
-            }
-        }
-    }
-}); 
+//             }
+//         }
+//     }
+// }); 
